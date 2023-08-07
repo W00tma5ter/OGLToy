888266fccaaa066445ea99488f726538c045f026
W00tma5ter <49281441+W00tma5ter@users.noreply.github.com>
2023-08-07 15:30:12 -0700

    moved shader compilation to external header file with support for shader file inputs.

--- HelloGL.cpp
+++ HelloGL.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <Shader.h>
 
 // Function definitions.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
-//Shader code.
-const char* vertexShaderSource = 
-"#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"uniform vec4 ourColor;\n"
-"out vec4 vertexColor;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"    vertexColor = ourColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = 
-"#version 460 core\n"
-"in vec4 vertexColor;\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vertexColor;\n"
-"}\0";
 
 //Window Size Variables.
 const unsigned int resolution_x = 1920;
@@ -67,65 +47,13 @@ int main()
         return -1;
     }
 
-
-
-    // Build and compile shader program.
-    // Error checking variables.
-    int  success;
-    char infoLog[512];
-    // Create vertex shader object.
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    // Link shader code, and compile.
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // Check for compile errors.
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Create fragment shader object.
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    // Link shader code, and compile.
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // Check for compile errors.
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Create shader program object.
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    // Link shaders to program.
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // Check for program link errors.
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    // Cleanup shaders after linking.
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-
-
     // Vertex and index data.
     float vertices[] = 
     {
-     0.5f,  0.5f, 0.0f,     // top right
-     0.5f, -0.5f, 0.0f,     // bottom right
-    -0.5f, -0.5f, 0.0f,     // bottom left
-    -0.5f,  0.5f, 0.0f      // top left 
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
     unsigned int indices[] = 
     {
@@ -151,16 +79,28 @@ int main()
         GL_STATIC_DRAW      // GPU draw type.
     );
     // Set vertex attributes and enable.
+    // Postion attribute.
     glVertexAttribPointer
     (
         0,                  // vertex attribute.
         3,                  // vertex size.
         GL_FLOAT,           // data type.
         GL_FALSE,           // normalize data.
-        3 * sizeof(float),  // size of data.
+        6 * sizeof(float),  // size of data.
         (void*)0            // ofset (type void).
     );
     glEnableVertexAttribArray(0);
+    // Color attribute.
+    glVertexAttribPointer
+    (
+        1,                          // vertex attribute.
+        3,                          // vertex size.
+        GL_FLOAT,                   // data type.
+        GL_FALSE,                   // normalize data.
+        6 * sizeof(float),          // size of data.
+        (void*)(3 * sizeof(float))  // attribute ofset (type void).
+    );
+    glEnableVertexAttribArray(1);
 
     // Create element buffer object and bind to buffers.
     unsigned int EBO;
@@ -175,7 +115,7 @@ int main()
         GL_STATIC_DRAW              // GPU draw type.
     );
 
-
+    Shader ourShader("../Shaders/shader.verts", "../Shaders/shader.frags");
 
     // Render Loop (frame).
     while (!glfwWindowShouldClose(window))
@@ -189,21 +129,15 @@ int main()
             glClear(GL_COLOR_BUFFER_BIT);
 
             // Draw triangle.
-            glUseProgram(shaderProgram);
-            glBindVertexArray(VAO);
-
-            //Update shape color.
-            float timeValue = glfwGetTime();
-            float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-            int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-            glUseProgram(shaderProgram);
-            glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+            ourShader.use();
 
             // Draw a triangle using VBO vertices.
-            //glDrawArrays(GL_TRIANGLES, 0, 3);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
             // Draw a rectangle using EBO indices.
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            
             glBindVertexArray(0);
         }
 
